7a5afbddac087989f7623e62e24aae21f78cc1c3
mnindrazaka <mnindrazaka@gmail.com>
2018-06-03 14:45:23 +0700

    draw damaged road

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const {Client} = require('pg')
+const { Client } = require("pg")
 
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'OSM',
-  password: 'postgres',
-  port: 5433
+	user: "postgres",
+	host: "localhost",
+	database: "OSM",
+	password: "postgres",
+	port: 5433,
 })
 client.connect()
 
 // setting view engine
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+app.set("views", "views")
+app.set("view engine", "ejs")
 
 // serve static files
-app.use('/public', express.static('public'))
+app.use("/public", express.static("public"))
 
 // routing
-app.get('/', function (req, res) {
-  client.query('SELECT ST_AsGeoJSON(geom) FROM lines_jalan_jatim LIMIT 1000', function (err, result) {
-    let geojson = result.rows;
-    geojson = geojson.map(function (row) {
-      return JSON.parse(row.st_asgeojson)
-    })
+app.get("/", function(req, res) {
+	client.query(
+		"SELECT st_asgeojson(lgeom), color FROM pnm_damaged_roads a INNER JOIN pnm_road_segments b ON(a.sid = b.sid) INNER JOIN damage_type c ON (a.type_id = c.type_id) LIMIT 5000",
+		function(err, result) {
+			let geojson = result.rows
+			geojson = geojson.map(function(row) {
+				return {
+					...JSON.parse(row.st_asgeojson),
+					color: row.color,
+				}
+			})
 
-    client.query('SELECT ST_AsGeoJSON(geom) FROM lines_jalan_jatim LIMIT 5', function (err, result) {
-      let geojson2 = result.rows;
-      geojson2 = geojson2.map(function (row) {
-        return JSON.parse(row.st_asgeojson)
-      })
-
-      res.render('index', {geojson, geojson2})
-    })
-  })
+			console.log(geojson)
+			res.render("index", { geojson })
+		},
+	)
 })
 
 // start server
-app.listen(3000, function () {
-  console.log('Server running on port 3000')
+app.listen(3000, function() {
+	console.log("Server running on port 3000")
 })
