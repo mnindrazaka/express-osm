cff8a1088659dee8138be4954e2dcfdb18c254be
mnindrazaka <mnindrazaka@gmail.com>
2018-06-05 19:48:55 +0700

    filter damaged road

--- server.js
+++ server.js
@@ -19,25 +19,31 @@ app.set("view engine", "ejs")
 app.use("/public", express.static("public"))
 
 // routing
-app.get("/", function(req, res) {
-	client.query(
-		"SELECT st_asgeojson(lgeom), e.color, d.name as damage_level, c.name as damage_type, a.information FROM pnm_damaged_roads a INNER JOIN damage_type c ON (a.type_id = c.type_id) INNER JOIN damage_level d ON (a.level_id = d.level_id) INNER JOIN damage_color e ON (a.type_id = e.type_id AND a.level_id = e.level_id)",
-		function(err, result) {
-			let geojson = result.rows
-			geojson = geojson.map(function(row) {
-				return {
-					...JSON.parse(row.st_asgeojson),
-					color: row.color,
-					damage_level: row.damage_level,
-					damage_type: row.damage_type,
-					information: row.information,
-				}
-			})
-
-			console.log(geojson)
-			res.render("index", { geojson })
-		},
+app.get("/", async function(req, res) {
+	let geojson = await client.query(
+		"SELECT st_asgeojson(lgeom), e.color, d.name as damage_level, d.level_id as damage_level_id, c.name as damage_type, c.type_id as damage_type_id, a.information FROM pnm_damaged_roads a INNER JOIN damage_type c ON (a.type_id = c.type_id) INNER JOIN damage_level d ON (a.level_id = d.level_id) INNER JOIN damage_color e ON (a.type_id = e.type_id AND a.level_id = e.level_id)",
 	)
+	geojson = geojson.rows
+	geojson = geojson.map(function(row) {
+		return {
+			...JSON.parse(row.st_asgeojson),
+			color: row.color,
+			damage_level: row.damage_level,
+			damage_type: row.damage_type,
+			information: row.information,
+			damage_type_id: row.damage_type_id,
+			damage_level_id: row.damage_level_id,
+		}
+	})
+	console.log(geojson)
+
+	let damage_type = await client.query("SELECT * from damage_type")
+	damage_type = damage_type.rows
+
+	let damage_level = await client.query("SELECT * from damage_level")
+	damage_level = damage_level.rows
+
+	res.render("index", { geojson, damage_type, damage_level })
 })
 
 // start server
